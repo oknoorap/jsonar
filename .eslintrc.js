eee7b86f5adc8821e539c6c985bce2573998e53a
Ribhararnus Pracutian <oknoorap@gmail.com>
2017-06-20 09:27:16 +0700

    fix: indent + parser version

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,24 +5,25 @@ module.exports = {
   },
   "extends": "eslint:recommended",
   "parserOptions": {
-    "sourceType": "module"
+    "sourceType": "module",
+    "ecmaVersion": 8
   },
   "rules": {
     "indent": [
-    "error",
-    2
+      "error",
+      2
     ],
     "linebreak-style": [
-    "error",
-    "unix"
+      "error",
+      "unix"
     ],
     "quotes": [
-    "error",
-    "single"
+      "error",
+      "single"
     ],
     "semi": [
-    "error",
-    "never"
+      "error",
+      "never"
     ]
   }
 };
