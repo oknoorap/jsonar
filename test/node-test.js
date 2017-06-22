417096bfd8c05db44f94d1cd2e1547935e57eb5b
Ribhararnus Pracutian <oknoorap@gmail.com>
2017-06-22 14:50:07 +0700

    fix: test

--- test/node-test.js
+++ test/node-test.js
@@ -4,4 +4,4 @@ const jsonar = require('../index')
 
 const exampleFile = path.join(__dirname, 'example.json')
 const json = readFileSync(exampleFile, 'ascii')
-console.log(jsonar.arrify(json, true)) // eslint-disable-line no-console
+console.log(jsonar.arrify(json, {prettify: true, quote: jsonar.quoteTypes.SINGLE})) // eslint-disable-line no-console
