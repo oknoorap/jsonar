bf2c712b3c9ce72b73ee3931b5049f788aa208fe
oknoorap <oknoorap@gmail.com>
2017-06-25 21:53:17 +0700

    add: trailing comma test

--- test/test.js
+++ test/test.js
@@ -39,6 +39,14 @@ test('json is valid PHP array (prettify) with single quote', async t => {
   })
 })
 
+test('json is valid PHP array (prettify) with trailing comma', async t => {
+  await jsonFile().then(result => {
+    /* eslint-disable quotes */
+    t.is(jsonar.arrify(result, {prettify: true, quote: jsonar.quoteTypes.SINGLE, trailingComma: true}), "array(\n\t'greetings' => 'Hello',\n\t'answers' => 42,\n\t'inception' => array(\n\t\t'nested' => array(\n\t\t\t'object' => true,\n\t\t),\n\t\t'array' => array(\n\t\t\t'string',\n\t\t\ttrue,\n\t\t\t100,\n\t\t\tarray(\n\t\t\t\t'inception' => true,\n\t\t\t),\n\t\t),\n\t),\n\t'playlist' => array(\n\t\tarray(\n\t\t\t'id' => 'DHyUYg8X31c',\n\t\t\t'desc' => 'Do Robots Deserve Rights? What if Machines Become Conscious?',\n\t\t),\n\t\tarray(\n\t\t\t'id' => 'ijFm6DxNVyI',\n\t\t\t'desc' => 'The Most Efficient Way to Destroy the Universe - False Vacuum',\n\t\t),\n\t),\n);")
+    /* eslint-enable */
+  })
+})
+
 test('json is valid PHP array (prettify) 4 indentation with space', async t => {
   await jsonFile().then(result => {
     t.is(jsonar.arrify(result, {prettify: true, indent: 4, space: true}), 'array(\n    "greetings" => "Hello",\n    "answers" => 42,\n    "inception" => array(\n        "nested" => array(\n            "object" => true\n        ),\n        "array" => array(\n            "string",\n            true,\n            100,\n            array(\n                "inception" => true\n            )\n        )\n    ),\n    "playlist" => array(\n        array(\n            "id" => "DHyUYg8X31c",\n            "desc" => "Do Robots Deserve Rights? What if Machines Become Conscious?"\n        ),\n        array(\n            "id" => "ijFm6DxNVyI",\n            "desc" => "The Most Efficient Way to Destroy the Universe - False Vacuum"\n        )\n    )\n);')
