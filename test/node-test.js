b111ebf316abafd317d469b980db2c2dd69809b3
Ribhararnus Pracutian <oknoorap@gmail.com>
2017-08-01 17:00:45 +0700

    rename: node-test to playground

--- test/node-test.js
+++
@@ -1,7 +0,0 @@
-const {readFileSync} = require('fs')
-const path = require('path')
-const jsonar = require('../index')
-
-const exampleFile = path.join(__dirname, 'example.json')
-const json = readFileSync(exampleFile, 'ascii')
-console.log(jsonar.arrify(json, {prettify: true, quote: jsonar.quoteTypes.SINGLE})) // eslint-disable-line no-console
---
+++ test/playground.js
@@ -0,0 +1,18 @@
+const {readFileSync} = require('fs')
+const path = require('path')
+const jsonar = require('../index')
+
+const exampleFile = path.join(__dirname, 'example.json')
+const json = readFileSync(exampleFile, 'ascii')
+
+/* eslint-disable no-console */
+const output = jsonar.arrify(json, {prettify: true, quote: jsonar.quoteTypes.SINGLE})
+console.log(output)
+
+const secondJson = Object.assign({}, JSON.parse(json))
+secondJson.fn = jsonar.literal('__( \'Hello World\' , \'text-domain\' )')
+
+const secondOutput = jsonar.arrify(secondJson, {prettify: true, quote: jsonar.quoteTypes.SINGLE})
+console.log(secondOutput)
+console.log('parsing', JSON.stringify(jsonar.parse(secondOutput), null, 2))
+/* eslint-enable */
